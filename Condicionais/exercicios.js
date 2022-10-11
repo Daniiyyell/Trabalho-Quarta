835b520a8878f6eb93750d6ed9335b36fe33b001
Daniiyyell <111388063+Daniiyyell@users.noreply.github.com>
2022-10-11 14:34:37 -0300

    Add files via upload

---
+++ Condicionais/exercicios.js
@@ -0,0 +1,11 @@
+//1.0 crie um programa que le um valor e imprima dizendo se ele é maior ou menor que 10.
+
+var valor = prompt ('Digite um valor: ')
+
+if (valor > 10){
+    console.log('Maior')
+}else{
+    console.log('Menor')
+}
+
+//
---
+++ Condicionais/script.js
@@ -0,0 +1,47 @@
+//Condicionais if e else
+
+var GraziPossuiGraduacao = true
+
+/*if (GraziPossuiGraduacao){
+    console.log('Grazi está na faculdade')
+}else{
+    console.log('Grazi está na faculdade')
+}
+
+var idade = prompt('Digite a sua idade: ')
+
+if (idade >= 18){
+    console.log('Vai preso')
+}else{
+    console.log('Os pais vão ser preso')
+}
+
+//Operadores logicos
+
+
+        ==  -> igualdade
+        >   -> maior que
+        <   -> menor que
+        !=  -> diferente
+        >=  -> maior ou igual
+        <=  -> menor ou igual
+*/
+
+var a = 1
+    b = 5,
+    c = 2,
+    d = 1;
+
+console.log(a>b) //false
+
+console.log(a<b) //true
+
+console.log(a == d) //true
+
+console.log(b >= a) //true
+
+console.log(c <= b) //true
+
+console.log(d != a) //false
+
+console.log(d != b) //true
